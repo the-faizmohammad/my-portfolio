d5d1ccfe386b9c8aeed1d125365190c6eb94837c
user-faiz <email2faizm@gmail.com>
2023-06-27 03:07:26 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-
+function toggleMobileMenu() {
+    var mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.style.display = (mobileMenu.style.display === 'block') ? 'none' : 'block';
+  }
