3d94b555b07cbb812353f4aecaebe40b5ff78fb2
user-faiz <email2faizm@gmail.com>
2023-06-27 21:49:57 +0530

    update index.html

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ const navShow = document.getElementById("show-nav");
 const navHide = document.getElementById("hide-nav");
 const mobMenuLinks = document.querySelectorAll("#mobmenu-links li a");
 const mobileMenu = document.getElementById("mobile-menu");
-//Aamer code
 navShow.addEventListener("click", () => {
   mobileMenu.classList.toggle("mobile-menu--show");
 });
 
-
 navHide.addEventListener("click", () => {
   mobileMenu.classList.toggle("mobile-menu--show");
 });
-//Aamer code
 
 mobMenuLinks.forEach((link) => {
   link.addEventListener("click", (event) => {
