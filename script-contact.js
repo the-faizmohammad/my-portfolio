842d7288b41b7adbee0fd51d828afba5ac5fe64f
user-faiz <email2faizm@gmail.com>
2023-06-30 02:21:07 +0530

    update script-contact.js

--- script-contact.js
+++ script-contact.js
@@ -2,4 +2,10 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     var emailInput = document.getElementsByName('email')[0];
     var errorMessage = document.getElementById('error-message');
+    if (emailInput.value !== emailInput.value.toUpperCase()) { //Yaser Commit
+        event.preventDefault();
+        errorMessage.textContent = 'Faiz contact validation';// Binyam commit
+      } else {
+        errorMessage.textContent = '';
+      }
 })
