a82dfa9d1b371c4a5627671fd85fcfb18f4af805
user-faiz <email2faizm@gmail.com>
2023-06-30 02:16:55 +0530

    update script-contact.js

--- script-contact.js
+++ script-contact.js
@@ -1,3 +1,5 @@
 // this is contact form validation
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-}
+    var emailInput = document.getElementsByName('email')[0];
+    var errorMessage = document.getElementById('error-message');
+})
