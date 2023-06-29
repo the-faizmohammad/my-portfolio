f35324e62202da0d868a73e89331ea9b0b021595
user-faiz <email2faizm@gmail.com>
2023-06-29 17:05:41 +0530

    update style.css

--- popup.js
+++ popup.js
@@ -1,59 +1,59 @@
 const projects = [
-    {
-      featuredImg: './assets/Snapshoot Portfolio9.png',
-      name: 'Tonic',
-      Description:
+  {
+    featuredImg: './assets/Snapshoot Portfolio9.png',
+    name: 'Tonic',
+    Description:
         'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-      language: ['HTML', 'CSS', 'JavaScript'],
-      details: { company: 'CANOPY', profile: 'Back END Dev', year: 2015 },
-      seeLive: 'https://github.com/the-faizmohammad/my-portfolio',
-      seeSource: 'https://github.com/the-faizmohammad/my-portfolio.git',
-    },
-    {
-      featuredImg: './assets/Snapshoot Portfolio (5).png',
-      name: 'Multi-Post Stories',
-      Description:
+    language: ['HTML', 'CSS', 'JavaScript'],
+    details: { company: 'CANOPY', profile: 'Back END Dev', year: 2015 },
+    seeLive: 'https://github.com/the-faizmohammad/my-portfolio',
+    seeSource: 'https://github.com/the-faizmohammad/my-portfolio.git',
+  },
+  {
+    featuredImg: './assets/Snapshoot Portfolio (5).png',
+    name: 'Multi-Post Stories',
+    Description:
         'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-      language: ['HTML', 'CSS', 'Ruby', 'JavaScript'],
-      details: {
-        company: 'FACEBOOK',
-        profile: 'Full Stack Dev',
-        year: 2016,
-      },
-      seeLive: 'https://github.com/the-faizmohammad/my-portfolio',
-      seeSource: 'https://github.com/the-faizmohammad/my-portfolio.git',
+    language: ['HTML', 'CSS', 'Ruby', 'JavaScript'],
+    details: {
+      company: 'FACEBOOK',
+      profile: 'Full Stack Dev',
+      year: 2016,
     },
-    {
-      featuredImg: './assets/Snapshoot Portfolio.png',
-      name: 'Facebook 360',
-      Description:
+    seeLive: 'https://github.com/the-faizmohammad/my-portfolio',
+    seeSource: 'https://github.com/the-faizmohammad/my-portfolio.git',
+  },
+  {
+    featuredImg: './assets/Snapshoot Portfolio.png',
+    name: 'Facebook 360',
+    Description:
         'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-      language: ['HTML', 'CSS', 'JavaScript', 'Ruby'],
-      details: {
-        company: 'FACEBOOK',
-        profile: 'Full Stack Dev',
-        year: 2017,
-      },
-      seeLive: 'https://github.com/the-faizmohammad/my-portfolio',
-      seeSource: 'https://github.com/the-faizmohammad/my-portfolio.git',
+    language: ['HTML', 'CSS', 'JavaScript', 'Ruby'],
+    details: {
+      company: 'FACEBOOK',
+      profile: 'Full Stack Dev',
+      year: 2017,
     },
-    {
-      featuredImg: './assets/Snapshoot Portfolio (2).png',
-      name: 'Uber Navigation',
-      Description:
+    seeLive: 'https://github.com/the-faizmohammad/my-portfolio',
+    seeSource: 'https://github.com/the-faizmohammad/my-portfolio.git',
+  },
+  {
+    featuredImg: './assets/Snapshoot Portfolio (2).png',
+    name: 'Uber Navigation',
+    Description:
         'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-      language: ['HTML', 'Ruby', 'CSS', 'JavaScript'],
-      details: { company: 'UBER', profile: 'Front End Dev', year: 2017 },
-      seeLive: 'https://github.com/the-faizmohammad/my-portfolio',
-      seeSource: 'https://github.com/the-faizmohammad/my-portfolio.git',
-    },
-  ];
+    language: ['HTML', 'Ruby', 'CSS', 'JavaScript'],
+    details: { company: 'UBER', profile: 'Front End Dev', year: 2017 },
+    seeLive: 'https://github.com/the-faizmohammad/my-portfolio',
+    seeSource: 'https://github.com/the-faizmohammad/my-portfolio.git',
+  },
+];
 
-  function createCard(project, index) {
-    const mycard = document.createElement('li');
-    mycard.classList.add('my-works-card');
-    mycard.id = `my-works-card-${index + 1}`;
-    mycard.innerHTML = `
+function createCard(project, index) {
+  const mycard = document.createElement('li');
+  mycard.classList.add('my-works-card');
+  mycard.id = `my-works-card-${index + 1}`;
+  mycard.innerHTML = `
       <img src='${project.featuredImg}' alt='Recent Work'>
       <div class='works-card-main'>
           <h3 class='works-card-heading-large'>${project.name}</h3>
@@ -66,18 +66,18 @@ const projects = [
           </div>
           <p class='paragraph works-card-paragraph'>${project.Description}</p>
           <ul class="works-card-skills">
-              ${project.language.map(skill => `<li class="skill-list">${skill}</li>`).join('')}
+              ${project.language.map((skill) => `<li class="skill-list">${skill}</li>`).join('')}
           </ul>
           <button onclick="showProjectDetails(${index + 1})">See Project</button>
       </div>`;
-    return mycard;
-  }
-  
-  function createPopup(project, index) {
-    const popup = document.createElement('div');
-    popup.classList.add('card-detail');
-    popup.id = `card-detail-${index + 1}`;
-    popup.innerHTML = `
+  return mycard;
+}
+
+function createPopup(project, index) {
+  const popup = document.createElement('div');
+  popup.classList.add('card-detail');
+  popup.id = `card-detail-${index + 1}`;
+  popup.innerHTML = `
       <div class='card-detail-inner'>
         <div class='d-flex card-detail-header'>
             <div>
@@ -99,7 +99,7 @@ const projects = [
             <p class='paragraph'>${project.Description}</p>
             <div class='card-detail-body-right'>
                 <ul class="d-flex card-detail-body-tags">
-                  ${project.language.map(skill => `<li class="card-detail-body-tag">${skill}</li>`).join('')}
+                  ${project.language.map((skill) => `<li class="card-detail-body-tag">${skill}</li>`).join('')}
                 </ul>
                 <div class='card-detail-body-right-button-container'>
                     <button class='card-detail-button' src='${project.seeLive}'>
@@ -112,29 +112,29 @@ const projects = [
             </div>
         </div>
       </div>`;
-    return popup;
-  }
-  
-  function showProjectDetails(index) {
-    const popup = document.getElementById(`card-detail-${index}`);
-    const body = document.getElementById('body');
-    popup.classList.add('card-detail--show');
-    body.classList.add('blur');
-  }
-  
-  function hideProjectDetails(index) {
-    const popup = document.getElementById(`card-detail-${index}`);
-    const body = document.getElementById('body');
-    popup.classList.remove('card-detail--show');
-    body.classList.remove('blur');
-  }
-  
-  window.onload = function () {
-    const container = document.getElementById('works-cards');
-    const popupContainer = document.getElementById('popup-container');
-  
-    projects.forEach((project, index) => {
-      container.appendChild(createCard(project, index));
-      popupContainer.appendChild(createPopup(project, index));
-    });
-  };
+  return popup;
+}
+
+function showProjectDetails(index) {
+  const popup = document.getElementById(`card-detail-${index}`);
+  const body = document.getElementById('body');
+  popup.classList.add('card-detail--show');
+  body.classList.add('blur');
+}
+
+function hideProjectDetails(index) {
+  const popup = document.getElementById(`card-detail-${index}`);
+  const body = document.getElementById('body');
+  popup.classList.remove('card-detail--show');
+  body.classList.remove('blur');
+}
+
+window.onload = function LoadPopup() {
+  const container = document.getElementById('works-cards');
+  const popupContainer = document.getElementById('popup-container');
+
+  projects.forEach((project, index) => {
+    container.appendChild(createCard(project, index));
+    popupContainer.appendChild(createPopup(project, index));
+  });
+};
