5491e9618e6462db9e94e80d774559030635911c
user-faiz <email2faizm@gmail.com>
2023-06-30 14:20:13 +0530

    update script-contact.js

--- script-contact.js
+++ script-contact.js
@@ -1,11 +1,11 @@
-// this is contact form validation
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     var emailInput = document.getElementsByName('email')[0];
     var errorMessage = document.getElementById('error-message');
-    if (emailInput.value !== emailInput.value.toUpperCase()) { //Yaser Commit
-        event.preventDefault();
-        errorMessage.textContent = 'Faiz contact validation';// Binyam commit
-      } else {
-        errorMessage.textContent = '';
-      }
-})
+    
+    if (emailInput.value !== emailInput.value.toUpperCase()) { // Make this Lower case
+      event.preventDefault();
+      errorMessage.textContent = 'Silly Error.';
+    } else {
+      errorMessage.textContent = '';
+    }
+  });
