07f079d0245e572c2f6cb19e636ff5c467ff9efa
user-faiz <email2faizm@gmail.com>
2023-06-30 16:16:28 +0530

    corrected eslint error

--- script-contact.js
+++ script-contact.js
@@ -1,11 +1,11 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    var emailInput = document.getElementsByName('email')[0];
-    var errorMessage = document.getElementById('error-message');
-    
-    if (emailInput.value !== emailInput.value.toLowerCase()) { // Make this Lower case
-      event.preventDefault();
-      errorMessage.textContent = 'Please enter your email in lowercase.';
-    } else {
-      errorMessage.textContent = '';
-    }
-  });
+document.getElementById('contact-form').addEventListener('submit', (event) => {
+  const emailInput = document.getElementsByName('email')[0];
+  const errorMessage = document.getElementById('error-message');
+
+  if (emailInput.value !== emailInput.value.toLowerCase()) { // Make this Lower case
+    event.preventDefault();
+    errorMessage.textContent = 'Please enter your email in lowercase.';
+  } else {
+    errorMessage.textContent = '';
+  }
+});
